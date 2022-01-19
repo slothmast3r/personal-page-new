8ccc07c25ccdc5df47717dc37bf1868780c67b97
slothmast3r <oskar.straszynski@gmail.com>
2022-01-19 23:08:01 +0100

    paragraph

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,16 @@
       <nav-bar
         :tabs="mainTabs"
       />
-      <personal-button :button-type="'secondary'">Resume </personal-button>
+      <personal-button :button-type="'secondary'">
+        Resume
+      </personal-button>
     </div>
     <introduction />
+    <paragraph
+      :name-section="'What I do?'"
+      :title="'Better design, \n Better experience'"
+      :description="description"
+    />
   </div>
 </template>
 
@@ -15,9 +22,11 @@
 import Introduction from "@/components/Introduction";
 import NavBar from "@/components/NavBar";
 import PersonalButton from "@/components/PersonalButton";
+import Paragraph from "@/components/Paragraph";
 export default {
   name: 'App',
   components: {
+    Paragraph,
     PersonalButton,
     NavBar,
     Introduction
@@ -29,7 +38,8 @@ export default {
           {name: 'Projects', id: 'projects'},
           {name: 'About me', id: 'aboutMe'},
           {name: 'Contact', id: 'contact'},
-      ]
+      ],
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
     }
   }
 }
@@ -41,6 +51,7 @@ body{
   font-family: 'Rubik', sans-serif;
   color: $white-smoke;
   margin: 2rem 6rem;
+  font-size: 1rem;
 }
 #app {
 
---
+++ src/components/Paragraph.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="paragraph-wrapper">
+    <div class="section-wrapper">
+      <div class="line-section" />
+      <div class="section-text">
+        {{ nameSection }}
+      </div>
+    </div>
+    <div class="title-wrapper">
+      <div class="text-title">
+        {{ title }}
+      </div>
+    </div>
+    <div class="description-wrapper">
+      <div class="description-text">
+        {{ description }}
+      </div>
+    </div>
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Paragraph",
+  props:{
+    title:{
+      type: String,
+      default: '',
+    },
+    nameSection:{
+      type: String,
+      required: true
+    },
+    description:{
+      type: String,
+      default: '',
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.paragraph-wrapper{
+  .section-wrapper {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    text-transform: uppercase;
+    font-size: 1.3rem;
+    margin: 1rem 0;
+
+    .line-section {
+      width: 4rem;
+      height: 0.2rem;
+      background: $secondary-color;
+    }
+    .section-text{
+      opacity: 0.8;
+    }
+  }
+  .title-wrapper{
+    font-size: 2rem;
+    color: $white-smoke;
+    margin: 1.5rem 0;
+    font-weight: 700;
+  }
+  .description-wrapper{
+    line-height: 1.5rem;
+    opacity: 0.8;
+  }
+
+}
+</style>
