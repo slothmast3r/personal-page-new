b1dd3a5616bf1fc0cbdc387f6d5be810a2bdf172
slothmast3r <oskar.straszynski@gmail.com>
2022-01-19 16:35:42 +0100

    eslint

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    Hello! I'm
+    <div>      Hello! I'm
+    </div>
+    <div>
+      Oskar Straszyński
+    </div>
+    <div>
+      Frontend Developer
+    </div>
+    <button> get in touch</button>
   </div>
 </template>
 
