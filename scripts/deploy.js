b2755849831feb9821b0d869944b7b0b33816980
slothmast3r <oskar.straszynski@gmail.com>
2022-02-16 21:45:08 +0100

    xd

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,5 +1,7 @@
-const execa = require("execa");
-const fs = require("fs");
+// const execa = require("execa");
+// const fs = require("fs");
+import {execa} from "execa";
+import fs from "fs";
 (async () => {
     try {
         await execa("git", ["checkout", "--orphan", "gh-pages"]);
