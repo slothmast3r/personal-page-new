f3e15cb99f683a185b18c452b25a5f1ce4e41196
slothmast3r <oskar.straszynski@gmail.com>
2022-02-17 20:04:17 +0100

    about

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,7 @@
         Resume
       </personal-button>
     </div>
-    <router-view></router-view>
-
+    <router-view />
   </div>
 </template>
 
@@ -29,8 +28,8 @@ export default {
     return{
       mainTabs : [
           {name: 'Home', id: 'home'},
-          {name: 'Projects', id: 'projects'},
           {name: 'About me', id: 'aboutMe'},
+          {name: 'Projects', id: 'projects'},
           {name: 'Contact', id: 'contact'},
       ],
       description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
