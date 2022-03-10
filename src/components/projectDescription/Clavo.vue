9e98e8dead3632bcd6592e5684556fd23066ec3f
slothmast3r <oskar.straszynski@gmail.com>
2022-03-10 23:56:29 +0100

    clavo

--- src/components/projectDescription/Clavo.vue
+++ src/components/projectDescription/Clavo.vue
@@ -1,7 +1,26 @@
 <template>
-<div>
+  <div>
+    Startup company - a work-time scheduling for various teams. Clavo helps organise and coordinate the working hours of all team members. <br>
 
-</div>
+    My responsibilities were:
+    <ul>
+      <li>
+        Updating layouts to meet usability and performance requirements.
+      </li> <li>
+        Adding new website features in javascript (framework Vue)
+      </li> <li>
+        Producing websites compatible with multiple browsers or smartphones (Responsive web design).
+      </li> <li>
+        Daily code review
+      </li> <li>
+        Fixing bugs
+      </li> <li>
+        Taking care of high-quality code
+      </li> <li>
+        Optimizing already existing code
+      </li> <li />
+    </ul>
+  </div>
 </template>
 
 <script>
